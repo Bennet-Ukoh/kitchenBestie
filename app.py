673a229ba4c5c80a58757737fe867b943606bad5
Bennet Ukoh <bennetukoh@gmail.com>
2023-04-06 17:33:05 +0100

    Landingpage added

--- app.py
+++ app.py
@@ -14,7 +14,25 @@ def search():
     data = response.json()
     return jsonify(data)
 
+@app.route("/landingpage")
+def landingpage():
+    return render_template("landingpage.html")
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/favorites")
+def favorites():
+    return render_template("favorites.html")
     
 
 
